9abba89fde3ec7f812d07ca7442878be6c6d6c26
Javeria Shahzadi <javeriashahzadi6@gmail.com>
2020-08-11 23:46:59 +0500

    Create windows.py

---
+++ windows.py
@@ -0,0 +1,19 @@
+## setting up enviorement
+
+pip install virtualenv
+
+#creating
+virtualenv mypython   #mypython is foldername
+
+
+#activating
+.\env\Scripts\activate
+
+#dectivating
+deactivate
+
+##########################################################
+
+
+
+
